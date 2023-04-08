dfea77200751456a1cdd70d9fa83c01f63731fc7
kartikpatel12901 <124262784+kartikpatel12901@users.noreply.github.com>
2023-04-08 16:21:51 +0530

    Add files via upload

---
+++ task/templete/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#slider').cycle('shuffle');
+     // $('#slider').cycle('zoom');
+    // $('#slider').cycle('scrollRight');
+    // $('#slider').cycle('scrollLeft');
+    // $('#slider').cycle('fade');
+    //$('#slider').cycle('zoom, fade, scrollRight');
+})
